4ba7b1800b58c5d847eb00f4efbee22967075ec0
Ji Qi <92043159+jiqicn@users.noreply.github.com>
2023-04-11 16:58:52 +0200

    Change setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import find_packages, setup
 
 
+with open('README.md', 'r') as file:
+    LONG_DESC = file.read()
+
 INSTALL_REQUIRES = ['pandas', 'matplotlib']
 EXTRA_REQUIRES = ['pytest', 'jupyter']
 
@@ -9,6 +12,8 @@ setup(
     name="mhm",
     version='0.1.0',
     description="An agent-based simulation model to study mental health outcomes during covid-19 lockdowns.",
+    long_description=LONG_DESC, 
+    long_description_content_type='text/markdown', 
     url="https://github.com/covid19ABM/mhm",
     author="Eva Viviani, Ji Qi",
     packages=find_packages(exclude=["tests", ".github"]),
