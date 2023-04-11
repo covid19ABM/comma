e53746fee9d4c7af3776b4ff206e54c40c418765
Ji Qi <92043159+jiqicn@users.noreply.github.com>
2023-04-11 16:56:50 +0200

    Change setup.py.

--- setup.py
+++ setup.py
@@ -10,8 +10,6 @@ setup(
     version='0.1.0',
     description="An agent-based simulation model to study mental health outcomes during covid-19 lockdowns.",
     url="https://github.com/covid19ABM/mhm",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
     author="Eva Viviani, Ji Qi",
     packages=find_packages(exclude=["tests", ".github"]),
     python_requires='>=3.6', 
