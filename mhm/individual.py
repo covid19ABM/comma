25ef32460788bab36fce54413e0fe84b6240400c
Ji Qi <92043159+jiqicn@users.noreply.github.com>
2023-04-13 13:16:30 +0200

    Implement populate method.

--- mhm/individual.py
+++ mhm/individual.py
@@ -2,21 +2,22 @@
 """
 
 import pandas as pd
+import numpy as np
 
 class Individual:
-    _features = pd.DataFrame()
+    features = pd.DataFrame()
     
     def __init__(self):
         self.id: int = None
         self.status: dict = None
         
-    def setup(self):
+    def setup(self, id: int):
         """Setup an agent
 
         Args:
             id (int): _description_
         """
-        pass
+        self.id = id
         
     def action(self):
         """Perform action and compute mental effect
@@ -24,7 +25,13 @@ class Individual:
         pass
     
     @staticmethod
-    def populate(parameters: dict):
-        """Fill values in the _features matrix 
+    def populate(size: int, seed: int, **kwargs):
+        """Create a population with an arbitrary number of features
         """
-        pass
+        Individual.features.drop(Individual.features.index, inplace=True)
+        np.random.seed(seed)
+        for feature, distribution in kwargs.items():
+            Individual.features[feature] = np.random.choice(
+                distribution[0], size, p=distribution[1]
+            )
+        
