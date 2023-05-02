fbe23dc4ae0a3491d783f79c2e81f31af22f1336
Ji Qi <92043159+jiqicn@users.noreply.github.com>
2023-05-02 13:04:33 +0200

    Add package methods for creating empty hypothesis files.

---
+++ mhm/__init__.py
@@ -0,0 +1,52 @@
+import os
+import json
+import pandas as pd
+
+
+PARAMS_INDIVIDUAL = 'params_individual.json'
+PARAMS_MODEL = 'params_model.json'
+
+
+def read_json_as_dict(fpath: str):
+    """Read content of a JSON file as Python dictionary.
+
+    Args:
+        fpath (str): file path.
+
+    Returns:
+        dict: result dictionary.
+    """
+    assert os.path.isfile(fpath), 'File not found: %s.' % fpath
+    with open(fpath) as file:
+        data = json.load(file)
+    return data
+
+def create_empty_hypotheses(dir_params: str):
+    """Create empty hypotheses files in a user-defined folder.
+
+    Args:
+        dir_params (str): folder that contains the agent and model parameter files.
+    """
+    fpath_params_individual = os.path.join(dir_params, PARAMS_INDIVIDUAL)
+    fpath_params_model = os.path.join(dir_params, PARAMS_MODEL)
+    params_individual = read_json_as_dict(fpath_params_individual) 
+    params_model = read_json_as_dict(fpath_params_model)
+    
+    actions = params_model['actions']
+    lockdown_policies = params_model['lockdown_policies']
+    status = params_model['status']
+    columns = ['actions', 'baseline']
+    for key, value in params_individual.items():
+        # categorical columns will be one-hot encoded by value
+        if isinstance(value[0][0], str):
+            columns += [key + "_" + v for v in value[0]]
+        else:
+            columns += [key]
+    df = pd.DataFrame(0, index=range(len(actions)), columns=columns)
+    df['actions'] = actions
+    
+    output_fpaths = ['lockdown_%s.csv' % l for l in lockdown_policies]
+    output_fpaths += ['actions_effects_on_%s.csv' % s for s in status]
+    output_fpaths = [os.path.join(dir_params, fp) for fp in output_fpaths]
+    for fp in output_fpaths:
+        df.to_csv(fp, sep=';', index=False)
