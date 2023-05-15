e7b0ed97dae291c6f45de2610c2ae19c27615419
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2023-05-15 16:26:47 +0200

    docs updated

--- docs/conf.py
+++ docs/conf.py
@@ -28,5 +28,5 @@ autoapi_dirs = ['../comma']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'agogo'
+html_theme = 'alabaster'
 html_static_path = ['_static']
