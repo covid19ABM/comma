4cde01bea648cfa899b4d7255982abc9033ea29c
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2023-07-31 15:47:53 +0200

    made assert checks more robust

--- comma/model.py
+++ comma/model.py
@@ -8,9 +8,9 @@ import os
 
 class Model:
     _status = dict()
-    _required_params = ['size', 'steps', 'actions', 'status', 
-                       'lockdown_policies', 'lockdown']
-    
+    _required_params = ['size', 'steps', 'actions', 'status',
+                        'lockdown_policies', 'lockdown']
+
     def __init__(self):
         self.agents: list = None
         self.id: int = None
@@ -25,7 +25,7 @@ class Model:
         self.param_file_validation(dir_params)
         self.agents = Individual.populate(size, dir_params)
         return self.agents
-    
+
     def param_file_validation(self, dir_params: str):
         """Validate files in the parameter folder.
         
@@ -39,27 +39,27 @@ class Model:
             "Parameter file not found: %s." % PARAMS_INDIVIDUAL
         assert os.path.isfile(fpath_params_model), \
             "Parameter file not found: %s" % PARAMS_MODEL
-       
+
         # check if all required model parameters are given 
         params_model = read_json_as_dict(fpath_params_model)
         missing_model_params = set(self._required_params) - set(params_model.keys())
         assert not missing_model_params, \
             "Model parameter(s) not found: %s." % ", ".join(missing_model_params)
-            
+
         # check if there is any overlap between lockdown intervals
         lockdown = params_model["lockdown"]
-        lockdown_intervals = [l["interval"] for l in lockdown] 
+        lockdown_intervals = [l["interval"] for l in lockdown]
         lockdown_intervals = sorted(lockdown_intervals, key=lambda li: li[0])
-        overlaps = [lockdown_intervals[i][1] >= lockdown_intervals[i+1][0] \
-            for i in range(len(lockdown_intervals) - 1)]
+        overlaps = [lockdown_intervals[i][1] >= lockdown_intervals[i + 1][0] \
+                    for i in range(len(lockdown_intervals) - 1)]
         assert not any(overlaps), "Lockdown intervals have overlap: \n%s" % "\n".join(
-            ["%s - %s" % (str(lockdown_intervals[i]), str(lockdown_intervals[i+1])) \
-                for i in range(len(overlaps)) if overlaps[i]] 
+            ["%s - %s" % (str(lockdown_intervals[i]), str(lockdown_intervals[i + 1])) \
+             for i in range(len(overlaps)) if overlaps[i]]
         )
-        
+
         # check if all the steps are fully covered by the given lockdown intervals
         steps = params_model["steps"]
-        expected_timeline = list(range(1, steps+1))
+        expected_timeline = list(range(1, steps + 1))
         given_timeline = []
         for li in lockdown_intervals:
             given_timeline += list(range(li[0], li[1] + 1))
@@ -67,7 +67,7 @@ class Model:
         assert not uncovered_steps, "Uncovered steps: %s." % ", ".join(
             [str(us) for us in uncovered_steps]
         )
-            
+
         # check if all hypothesis files exist
         fnames = ["actions_effects_on_%s.csv" % s for s in params_model["status"]]
         fnames += ["lockdown_%s.csv" % l for l in params_model["lockdown_policies"]]
@@ -76,25 +76,30 @@ class Model:
         assert all(fexist), "Hypothesis file(s) not found: %s." % ", ".join(
             [fnames[i] for i in range(len(fnames)) if not fexist[i]]
         )
-        
+
         # check if all hypothesis files contain all the required agent features
         required_features = ["actions", "baseline"]
         required_features += _get_one_hot_encoded_features(fpath_params_individual)
-        hypothesis_data = [pd.read_csv(fp, sep=";") for fp in fpaths]
-        missing_features = [set(required_features) - set(hd.columns) for hd in hypothesis_data]
+        hypothesis_data = [pd.read_csv(fp, sep=";", decimal=",") for fp in fpaths]
+        missing_features = []
+        for hd in hypothesis_data:
+            # avoid raising an issue if the mismatch it's in the case (e.g., upper vs lower case)
+            missing_features.append(set([f.lower() for f in required_features]) \
+                                    - set([c.lower() for c in hd.columns]))
+
         assert not any(missing_features), "Missing features:\n%s" % "\n".join(
             ["%s - %s" % (fnames[i], ", ".join(missing_features[i])) \
-                for i in range(len(fnames)) if missing_features[i]]
+             for i in range(len(fnames)) if missing_features[i]]
         )
-        
+
         # check if all hypothesis files contain hypotheses of all actions
         required_actions = params_model["actions"]
         missing_actions = [set(required_actions) - set(hd["actions"]) for hd in hypothesis_data]
         assert not any(missing_actions), "Missing actions:\n%s" % "\n".join(
             ["%s - %s" % (fnames[i], ", ".join(missing_actions[i])) \
-                for i in range(len(fnames)) if missing_actions[i]]
+             for i in range(len(fnames)) if missing_actions[i]]
         )
-    
+
     def step(self, lockdown: str):
         """Actions to be performed in each step.
 
@@ -104,7 +109,7 @@ class Model:
         for agent in self.agents:
             actions, action_probs = agent.choose_actions_on_lockdown(lockdown)
             agent.take_actions(actions=actions)
-    
+
     def update(self, lockdown: str):
         """Data to be recorded in each step.
 
