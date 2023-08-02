4b10e36863f99e0adafb41440e5bf5c095670f39
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2023-08-02 19:18:30 +0200

    latest improves on the code

--- comma/model.py
+++ comma/model.py
@@ -4,6 +4,7 @@ from comma.individual import Individual
 import pandas as pd
 from . import read_json_as_dict, _get_one_hot_encoded_features, PARAMS_INDIVIDUAL, PARAMS_MODEL
 import os
+import numpy as np
 from tqdm import tqdm
 
 
@@ -14,17 +15,24 @@ class Model:
 
     def __init__(self):
         self.agents: list = None
-        self.id: int = None
+        self.simulation_id: int = None
+        self.current_step: int = 0 # keep track of the current simulation step
+        self.lockdown_status: dict = {}
 
-    def setup(self, size: int, dir_params: str):
+    def setup(self, size: int, dir_params: str, use_ipf: bool = False):
         """Setup the model with input parameters.
 
         Args:
             size (int): size of the population
             dir_params (str): dir to the folder containing hypothesis and parameter files.
+            use_ipf (bool): If true, use the populate_ipf() method to populate the agents
+                            If false, use the standard populate method.
         """
         self.param_file_validation(dir_params)
-        self.agents = Individual.populate(size, dir_params)
+        if use_ipf:
+            self.agents = Individual.populate_ipf(size, dir_params)
+        else:
+            self.agents = Individual.populate(size, dir_params)
         return self.agents
 
     def param_file_validation(self, dir_params: str):
@@ -108,17 +116,37 @@ class Model:
             lockdown (str): lockdown type
         """
         for agent in self.agents:
+            # choose actions based on lockdown
             actions, action_probs = agent.choose_actions_on_lockdown(lockdown)
+            # take those actions, and compute their effect on mental health
             agent.take_actions(actions=actions)
 
-    def update(self, lockdown: str):
-        """Data to be recorded in each step.
+    def update(self, lockdown: str, step: int):
+        """Update status at every step.
 
         Args:
             lockdown (str): lockdown type
+            step (int): step of the simulation
         """
-        agent_statuses = [(agent.id, *agent.get_status()) for agent in self.agents]
-        self._status[self.id] = (lockdown, agent_statuses)
+        if self.current_step == 0:
+            # it's step 0, i.e., simulation just started, so we simply store the agent's status
+            delta_mh = 0 # it's day 0, so no incremental change
+            self._status[step] = [(lockdown, agent.id, delta_mh, *agent.get_status()) for agent in self.agents]
+
+
+        else:
+            # from step 1++, sum the agent's status with the status from the previous step
+            agent_statuses = []
+            for agent in self.agents:
+                last_status = [status for status in self._status[step - 1] if status[1] == agent.id][0][-1]
+                delta_mh = agent.get_status()[0] # this is the incremental effect
+                mu, sigma = 0.002, 0.0005
+                # this is the baseline effect when no action is taken
+                # or when action effects are canceled out
+                baseline = np.random.normal(mu, sigma)
+                new_status = (lockdown, agent.id, delta_mh, (last_status + delta_mh) - baseline)
+                agent_statuses.append(new_status)
+            self._status[step] = agent_statuses
 
     def report(self, out_path: str):
         """Collect data recorded at the end of the simulation and export as csv file.
@@ -128,13 +156,14 @@ class Model:
         """
         status_data = []
 
-        for step_id, (lockdown, agent_statuses) in self._status.items():
+        for step_id, agent_statuses in self._status.items():
             for agent_status in agent_statuses:
-                agent_id, mh = agent_status
-                status_data.append([step_id, agent_id, lockdown, mh])
+                lockdown, agent_id, delta_mh, mh = agent_status
+                status_data.append([step_id, lockdown, agent_id, delta_mh, mh])
 
         # create a pandas dataframe to store data from `status_data`
-        status_df = pd.DataFrame(status_data, columns=['step_id', 'agent_id', 'lockdown', 'mh'])
+        status_df = pd.DataFrame(status_data, columns=['step_id', 'lockdown', 'agent_id',
+                                                       'delta_mental_health', 'cumulative_mental_health'])
 
         # Export to a csv
         status_df.to_csv(out_path, index=False, sep=";", decimal=",")
@@ -153,7 +182,9 @@ class Model:
             raise ValueError("The length of the lockdown list must be equal to the number of steps")
 
         for step, current_lockdown in tqdm(enumerate(lockdown), total=steps, desc="Running simulation"):
-            self.id = step
+            self.simulation_id = step
+            self.lockdown_status[step] = current_lockdown
             self.step(current_lockdown)
-            self.update(current_lockdown)
+            self.update(current_lockdown, step)
+            self.current_step += 1  # Increment the simulation step
         self.report(out_path)
