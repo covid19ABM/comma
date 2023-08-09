2998a89a938670fba8af64948063501d303f57ec
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2023-08-09 14:17:38 +0200

    corrected script based on pylint

--- comma/model.py
+++ comma/model.py
@@ -6,13 +6,14 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 
+
 class Model:
     cumulative_status = dict()
 
     def __init__(self):
         self.agents: list = None
         self.simulation_id: int = None
-        self.current_step: int = 0 # keep track of the current simulation step
+        self.current_step: int = 0  # keep track of the current simulation step
         self.lockdown_status: dict = {}
 
     def setup(self, size: int, dir_params: str, use_ipf: bool = False):
@@ -20,9 +21,11 @@ class Model:
 
         Args:
             size (int): size of the population
-            dir_params (str): dir to the folder containing hypothesis and parameter files.
-            use_ipf (bool): If true, use the populate_ipf() method to populate the agents
-                            If false, use the standard populate method.
+            dir_params (str): dir to the folder
+            containing hypothesis and parameter files.
+            use_ipf (bool): If true, use the populate_ipf()
+            method to populate the agents If false,
+            use the standard populate method.
         """
         Hypothesis.validate_param_file(dir_params)
         if use_ipf:
@@ -30,7 +33,6 @@ class Model:
         else:
             self.agents = Individual.populate(size, dir_params)
         return self.agents
-
 
     def step(self, lockdown: str):
         """Actions to be performed in each step.
@@ -52,27 +54,32 @@ class Model:
             step (int): step of the simulation
         """
         if self.current_step == 0:
-            # it's step 0, i.e., simulation just started, so we simply store the agent's status
-            delta_mh = 0 # it's day 0, so no incremental change
-            self.cumulative_status[step] = [(lockdown, agent.id, delta_mh, agent.get_status()) for agent in self.agents]
-
-
+            delta_mh = 0  # it's day 0, so no incremental change
+            self.cumulative_status[step] = [
+                (lockdown, agent.id, delta_mh, agent.get_status())
+                for agent in self.agents
+            ]
         else:
-            # from step 1++, sum the agent's status with the status from the previous step
+            # from step 1++, sum the agent's status with the previous status
             agent_statuses = []
             for agent in self.agents:
-                last_status = [status for status in self.cumulative_status[step - 1] if status[1] == agent.id][0][-1]
-                delta_mh = agent.get_status() # this is the incremental effect
+                last_status = \
+                    [status for status in self.cumulative_status[step - 1]
+                     if status[1] == agent.id][0][-1]
+                delta_mh = agent.get_status()  # this is the incremental effect
                 mu, sigma = 0.002, 0.0005
                 # this is the baseline effect when no action is taken
                 # or when action effects are canceled out
                 baseline = np.random.normal(mu, sigma)
-                new_status = (lockdown, agent.id, delta_mh, (last_status + delta_mh) - baseline)
+                new_status = (lockdown, agent.id, delta_mh,
+                              (last_status + delta_mh) - baseline)
                 agent_statuses.append(new_status)
             self.cumulative_status[step] = agent_statuses
 
     def report(self, out_path: str):
-        """Collect data recorded at the end of the simulation and export as csv file.
+        """
+        Collect data recorded at the end of the simulation
+        and export as csv file.
 
         Args:
             out_path (str): File path of the output file
@@ -85,8 +92,11 @@ class Model:
                 status_data.append([step_id, lockdown, agent_id, delta_mh, mh])
 
         # create a pandas dataframe to store data from `status_data`
-        status_df = pd.DataFrame(status_data, columns=['step_id', 'lockdown', 'agent_id',
-                                                       'delta_mental_health', 'cumulative_mental_health'])
+        status_df = pd.DataFrame(
+            status_data,
+            columns=['step_id', 'lockdown', 'agent_id',
+                     'delta_mental_health', 'cumulative_mental_health']
+        )
 
         # Export to a csv
         status_df.to_csv(out_path, index=False, sep=";", decimal=",")
@@ -102,9 +112,12 @@ class Model:
             out_path (str): File path of the output file
         """
         if len(lockdown) != steps:
-            raise ValueError("The length of the lockdown list must be equal to the number of steps")
+            raise ValueError("The length of the lockdown list \
+            must be equal to the number of steps")
 
-        for step, current_lockdown in tqdm(enumerate(lockdown), total=steps, desc="Running simulation"):
+        for step, current_lockdown in tqdm(enumerate(lockdown),
+                                           total=steps,
+                                           desc="Running simulation"):
             self.simulation_id = step
             self.lockdown_status[step] = current_lockdown
             self.step(current_lockdown)
