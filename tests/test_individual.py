72feac47e5261aa628c81838a34e691292008267
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2023-10-18 18:51:51 +0200

    code restyle ;

--- tests/test_individual.py
+++ tests/test_individual.py
@@ -6,21 +6,22 @@ from pathlib import Path
 
 
 class TestIndividual:
-
     @pytest.fixture
     def dir_params(self):
         return Path("parameters/")
 
     @pytest.fixture
     def expected_actions(self):
-        return np.array([
-            'maintain_physical_distance',
-            'stay_at_home',
-            'exercise',
-            'socialise',
-            'seek_help',
-            'negative_coping'
-        ])
+        return np.array(
+            [
+                "maintain_physical_distance",
+                "stay_at_home",
+                "exercise",
+                "socialise",
+                "seek_help",
+                "negative_coping",
+            ]
+        )
 
     @pytest.fixture
     def expected_status(self):
@@ -28,44 +29,45 @@ class TestIndividual:
 
     @pytest.fixture
     def lockdown(self):
-        return 'easy'
+        return "easy"
 
     @pytest.fixture
     def expected_cols(self):
         return [
-            'baseline',
-            'age_group__1',
-            'age_group__2',
-            'age_group__3',
-            'age_group__4',
-            'gender_f',
-            'gender_m',
-            'education_high',
-            'education_low',
-            'education_medium',
-            'unemployed_no',
-            'unemployed_yes',
-            'have_partner_no',
-            'have_partner_yes',
-            'depressed_no',
-            'depressed_yes',
-            'children_presence_no',
-            'children_presence_yes',
-            'housing_financial_difficulties_no',
-            'housing_financial_difficulties_yes',
-            'selfrated_health_average',
-            'selfrated_health_good',
-            'selfrated_health_poor',
-            'critical_job_no',
-            'critical_job_yes'
+            "baseline",
+            "age_group__1",
+            "age_group__2",
+            "age_group__3",
+            "age_group__4",
+            "gender_f",
+            "gender_m",
+            "education_high",
+            "education_low",
+            "education_medium",
+            "unemployed_no",
+            "unemployed_yes",
+            "have_partner_no",
+            "have_partner_yes",
+            "depressed_no",
+            "depressed_yes",
+            "children_presence_no",
+            "children_presence_yes",
+            "housing_financial_difficulties_no",
+            "housing_financial_difficulties_yes",
+            "selfrated_health_average",
+            "selfrated_health_good",
+            "selfrated_health_poor",
+            "critical_job_no",
+            "critical_job_yes",
         ]
 
     @pytest.fixture
     def seed(self):
         return np.random.SeedSequence(0)
 
-    def test_take_actions(self, dir_params, expected_actions,
-                          expected_status, lockdown, seed):
+    def test_take_actions(
+        self, dir_params, expected_actions, expected_status, lockdown, seed
+    ):
         """
         Test for the `take_actions` method of the individual class.
 
@@ -76,27 +78,27 @@ class TestIndividual:
         """
 
         # specify the actions
-        actions = np.array([False, True, True, True, True,
-                            False, True, True, False, False])
+        actions = np.array(
+            [False, True, True, True, True, False, True, True, False, False]
+        )
 
         # specify the actions effects
         actions_effects = Hypothesis.read_hypotheses(
-            dir_params,
-            set([lockdown]),
-            "actions"
+            dir_params, set([lockdown]), "actions"
         )
-        individual = Individual.populate(1, dir_params,
-                                         rng=np.random.default_rng(seed))
+        individual = Individual.populate(1, dir_params, rng=np.random.default_rng(seed))
         individual[0].take_actions(actions, actions_effects[lockdown])
 
         individual[0].chosen_actions = actions
         actual_actions = individual[0].get_actions()
         actual_status = individual[0]._status
 
-        assert np.all(actual_actions == expected_actions), \
-            'actions chosen should match the expected'
-        assert np.all(actual_status == expected_status), \
-            'status after action taken should be equal to 0.'
+        assert np.all(
+            actual_actions == expected_actions
+        ), "actions chosen should match the expected"
+        assert np.all(
+            actual_status == expected_status
+        ), "status after action taken should be equal to 0."
 
     def test_choose_actions_on_lockdown(self, dir_params, lockdown, seed):
         """
@@ -112,33 +114,31 @@ class TestIndividual:
         """
 
         current_lockdown = Hypothesis.read_hypotheses(
-            dir_params,
-            set([lockdown]),
-            "lockdown"
+            dir_params, set([lockdown]), "lockdown"
         )
         # Create an individual with id 0
         individual = Individual.populate(1, dir_params)
 
-        actions, action_probs = individual[0].\
-            choose_actions_on_lockdown(
-            current_lockdown[lockdown],
-            rng=np.random.default_rng(seed)
+        actions, action_probs = individual[0].choose_actions_on_lockdown(
+            current_lockdown[lockdown], rng=np.random.default_rng(seed)
         )
 
-        assert isinstance(actions, np.ndarray), \
-            'actions should be a numpy array'
-        assert isinstance(action_probs, np.ndarray), \
-            'action_probs should be a numpy array'
+        assert isinstance(actions, np.ndarray), "actions should be a numpy array"
+        assert isinstance(
+            action_probs, np.ndarray
+        ), "action_probs should be a numpy array"
 
-        assert len(actions) == 10, \
-            'actions array should have length 10'
-        assert np.all((actions == 0) | (actions == 1)), \
-            'all actions should be False or True'
+        assert len(actions) == 10, "actions array should have length 10"
+        assert np.all(
+            (actions == 0) | (actions == 1)
+        ), "all actions should be False or True"
 
-        assert np.all((0 <= action_probs) & (action_probs <= 1)), \
-            'all action_probs should be between 0 and 1'
-        assert len(action_probs) == len(actions), \
-            'action_probs and actions should have the same length'
+        assert np.all(
+            (0 <= action_probs) & (action_probs <= 1)
+        ), "all action_probs should be between 0 and 1"
+        assert len(action_probs) == len(
+            actions
+        ), "action_probs and actions should have the same length"
 
     def test_populate(self, dir_params, expected_cols, seed):
         """
@@ -149,17 +149,18 @@ class TestIndividual:
         """
 
         # Populate with one individual for the test
-        individuals = Individual.populate(1, dir_params,
-                                          rng=np.random.default_rng(seed))
+        individuals = Individual.populate(
+            1, dir_params, rng=np.random.default_rng(seed)
+        )
 
-        assert list(individuals[0]._features.index) == expected_cols, \
-            'Columns in Individual._features are incorrect'
+        assert (
+            list(individuals[0]._features.index) == expected_cols
+        ), "Columns in Individual._features are incorrect"
 
         for individual in individuals:
             assert np.all(
-                (individual.get_features() == 0) |
-                (individual.get_features() == 1)
-            ), 'Values in Individual._features should be either 0 or 1'
+                (individual.get_features() == 0) | (individual.get_features() == 1)
+            ), "Values in Individual._features should be either 0 or 1"
 
     def test_populate_ipf(self, dir_params, expected_cols, seed):
         """
@@ -174,30 +175,29 @@ class TestIndividual:
 
         df = Individual.populate_ipf(1, dir_params, rng=np.random.default_rng(seed))
 
-        assert list(df[
-                        0].get_features().index) == expected_cols, \
-            'Columns in the dataframe returned by populate_ipf() are incorrect'
+        assert (
+            list(df[0].get_features().index) == expected_cols
+        ), "Columns in the dataframe returned by populate_ipf() are incorrect"
 
         assert np.all(
             (df[0].get_features() == 0) | (df[0].get_features() == 1)
-        ).all(), 'Values in the dataframe returned ' \
-                 'by populate_ipf should be either 0 or 1'
+        ).all(), (
+            "Values in the dataframe returned "
+            "by populate_ipf should be either 0 or 1"
+        )
 
     def test_actions_when_positive(self, dir_params, lockdown, seed):
         """
         Test that modify_policy_when_infected returns 'stay at home' action
         """
-        policy = Hypothesis.read_hypotheses(
-            dir_params,
-            set([lockdown]),
-            'lockdown'
-        )
-        agent = Individual.populate_ipf(1, dir_params,
-                                        rng=np.random.default_rng(seed))
+        policy = Hypothesis.read_hypotheses(dir_params, set([lockdown]), "lockdown")
+        agent = Individual.populate_ipf(1, dir_params, rng=np.random.default_rng(seed))
         lockdown_new = agent[0].modify_policy_when_infected(policy[lockdown])
         _, action_probs = agent[0].choose_actions_on_lockdown(lockdown_new)
         action_out = agent[0].get_actions()
-        assert action_out[0] == 'stay_at_home', f"Expected 'stay at home' " \
-                                                f"got {action_out[0]}"
-        assert action_probs.round()[2] == 1, f"Expected Probability ~ 1 " \
-                                             f"got {action_probs.round()[2]}"
+        assert action_out[0] == "stay_at_home", (
+            f"Expected 'stay at home' " f"got {action_out[0]}"
+        )
+        assert action_probs.round()[2] == 1, (
+            f"Expected Probability ~ 1 " f"got {action_probs.round()[2]}"
+        )
--- tests/test_validate_csvs.py
+++ tests/test_validate_csvs.py
@@ -11,26 +11,33 @@ class TestCSVIntegrity:
     This is important for the subsequent matrix
     multiplications done in the model.
     """
+
     @pytest.fixture(scope="class")
     def initial_csv_data(self):
         directory = Path("parameters/")
-        csv_files = [f for f in os.listdir(directory) if
-                     (f.endswith('.csv') and
-                      (f.startswith('lockdown') or
-                       f.startswith('actions')))
-                     ]
+        csv_files = [
+            f
+            for f in os.listdir(directory)
+            if (
+                f.endswith(".csv")
+                and (f.startswith("lockdown") or f.startswith("actions"))
+            )
+        ]
 
-        if not list(directory.glob("lockdown*.csv")) + \
-                list(directory.glob("actions*.csv")):
-            raise ValueError(f"No CSV files found in the directory '"
-                             f"{str(directory.absolute())}'.")
+        if not list(directory.glob("lockdown*.csv")) + list(
+            directory.glob("actions*.csv")
+        ):
+            raise ValueError(
+                f"No CSV files found in the directory '"
+                f"{str(directory.absolute())}'."
+            )
 
         first_df = pd.read_csv(os.path.join(directory, csv_files[0]), sep=";")
 
         column_names = first_df.columns.str.lower()
         num_columns = len(column_names)
         num_rows = len(first_df)
-        actions = set(first_df['actions'].unique())
+        actions = set(first_df["actions"].unique())
 
         return {
             "directory": directory,
@@ -38,31 +45,29 @@ class TestCSVIntegrity:
             "column_names": column_names,
             "num_columns": num_columns,
             "num_rows": num_rows,
-            "actions": actions
+            "actions": actions,
         }
 
     def test_input_example_matrices(self, initial_csv_data):
-
         for file in initial_csv_data["csv_files"][1:]:
-            df = pd.read_csv(
-                os.path.join(initial_csv_data["directory"], file),
-                sep=";"
-            )
+            df = pd.read_csv(os.path.join(initial_csv_data["directory"], file), sep=";")
             df.columns = df.columns.str.lower()
             # Check for the same number of columns
-            assert len(df.columns) == initial_csv_data["num_columns"], \
-                f"{file} has a different number of columns."
+            assert (
+                len(df.columns) == initial_csv_data["num_columns"]
+            ), f"{file} has a different number of columns."
 
             # Check for the same number of rows
-            assert len(df) == initial_csv_data["num_rows"], \
-                f"{file} has a different number of rows."
+            assert (
+                len(df) == initial_csv_data["num_rows"]
+            ), f"{file} has a different number of rows."
 
             # Check for the same column names
             if set(df.columns) != set(initial_csv_data["column_names"]):
-                missing = set(initial_csv_data["column_names"]).\
-                    difference(df.columns)
-                additional = set(df.columns).\
-                    difference(initial_csv_data["column_names"])
+                missing = set(initial_csv_data["column_names"]).difference(df.columns)
+                additional = set(df.columns).difference(
+                    initial_csv_data["column_names"]
+                )
                 raise AssertionError(
                     f"{file} has different column names. "
                     f"Missing: {missing}. Additional: {additional}"
@@ -70,17 +75,18 @@ class TestCSVIntegrity:
 
             # Check for the same column order
             if list(df.columns) != list(initial_csv_data["column_names"]):
-                diff_order = [i for i, (col_df, col_base) in
-                              enumerate(zip(df.columns,
-                                            initial_csv_data["column_names"])
-                                        ) if
-                              col_df != col_base]
+                diff_order = [
+                    i
+                    for i, (col_df, col_base) in enumerate(
+                        zip(df.columns, initial_csv_data["column_names"])
+                    )
+                    if col_df != col_base
+                ]
                 raise AssertionError(
-                    f"{file} has different column "
-                    f"order at indices: {diff_order}."
+                    f"{file} has different column " f"order at indices: {diff_order}."
                 )
 
             # Check for the same "actions" entries
-            assert set(
-                df['actions'].unique()) == initial_csv_data["actions"], \
-                f"{file} has different 'actions' entries."
+            assert (
+                set(df["actions"].unique()) == initial_csv_data["actions"]
+            ), f"{file} has different 'actions' entries."
