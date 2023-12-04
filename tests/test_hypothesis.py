8ddef8e7162f171602289e94f6599e2d2c4173ed
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2023-12-04 15:16:04 +0100

    test scaling function adjusted to deal with cumulative data

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -21,112 +21,52 @@ class TestHypothesis:
     def mock_df(self):
         return pd.DataFrame(
             {
-                "Version": [2],
-                "Date_of_report": ["2022-01-03T09:00:00Z"],
-                "Date_of_statistics": ["2022-01-01"],
-                "Security_region_code": ["VR01"],
-                "Security_region_name": ["Groningen"],
-                "Tested_with_result": [976],
-                "Tested_positive": [378],
+                "Version": [8, 8],
+                "Date_of_report": ["2022-01-01", "2022-01-02"],
+                "Date_of_publication": ["2022-01-01", "2022-01-02"],
+                "Municipality_code": ["GM0289", "GM0289"],
+                "Municipality_name": ["Wageningen", "Wageningen"],
+                "Total_reported": [6001, 6031],
             }
         )
 
-    @pytest.fixture
-    def file_list(self):
-        return [
-            "data-rivm/tests/rivm_daily_2021-01-01.csv.gz",
-            "data-rivm/tests/rivm_daily_2021-01-02.csv.gz",
-            "data-rivm/tests/rivm_daily_2021-01-15.csv.gz",
-            "data-rivm/tests/rivm_daily_2021-01-30.csv.gz",
-            "data-rivm/tests/rivm_daily_2021-02-01.csv.gz",
-            "data-rivm/tests/rivm_daily_2021-02-15.csv.gz",
-            "data-rivm/tests/rivm_daily_2021-02-28.csv.gz",
-        ]
-
-    def test_filter_dates_within_range(self, file_list):
-        # Test whether function correctly filter out dates
-        # that are *clearly* within the bounds of `time_period`
-        start = "2021-01-01"
-        steps = 2
-        expected_output = [
-            "data-rivm/tests/rivm_daily_2021-01-01.csv.gz",
-            "data-rivm/tests/rivm_daily_2021-01-02.csv.gz",
-        ]
-        hypothesis_instace = Hypothesis(start, steps)
-        output = hypothesis_instace.filter_dates(file_list)
-        assert output == expected_output
-
-    def test_filter_dates_on_boundary(self, file_list):
-        # Test whether function correctly includes
-        # or excludes dates that are exactly on the edge
-        start = "2021-01-16"
-        steps = 43
-        expected_output = [
-            "data-rivm/tests/rivm_daily_2021-01-30.csv.gz",
-            "data-rivm/tests/rivm_daily_2021-02-01.csv.gz",
-            "data-rivm/tests/rivm_daily_2021-02-15.csv.gz",
-            "data-rivm/tests/rivm_daily_2021-02-28.csv.gz",
-        ]
-        hypothesis_instance = Hypothesis(start, steps)
-        output = hypothesis_instance.filter_dates(file_list)
-        assert output == expected_output
-
-    def test_range_error(self, file_list):
+    def test_range_error(self):
         # Test that values outside the bounds raise an error
-        start = "2021-02-01"
+        start = "2025-02-01"
         steps = 1860
+        municipality_code = "GM0289"
         hypothesis_instance = Hypothesis(start, steps)
         with pytest.raises(
             ValueError, match=r"time_period .* is outside available dates"
         ):
-            hypothesis_instance.filter_dates(file_list)
+            hypothesis_instance.get_covid_data(municipality_code)
 
-    @patch("comma.hypothesis.Hypothesis.get_file_paths")
-    @patch("comma.hypothesis.Hypothesis.filter_dates")
+    @patch("comma.hypothesis.Hypothesis.download_covid_data")
+    @patch("comma.hypothesis.os.path.exists")
     @patch("comma.hypothesis.pd.read_csv")
-    # Mock tqdm to just return an iterator
-    @patch("comma.hypothesis.tqdm", side_effect=lambda x, *args, **kwargs: x)
-    def test_get_covid_data(
-        self,
-        mocked_tqdm,
-        mocked_read_csv,
-        mocked_filter_dates,
-        mocked_get_file_paths,
-        mock_df,
-    ):
-        # Test that the whole data extraction
-        location = "Groningen"
+    def test_get_covid_data(self, mock_read_csv, mock_exists, mock_download, mock_df):
+        # Test that the whole data extraction works
+        mock_exists.return_value = False
+        municipality_code = "GM0289"
         start = "2022-01-01"
-        steps = 2
-
-        # mock responses
-        mocked_get_file_paths.return_value = ["some_path"]
-        mocked_filter_dates.return_value = ["filtered_path"]
-        mocked_read_csv.return_value = mock_df
+        steps = 1
 
         hypothesis_instance = Hypothesis(start, steps)
-        df = hypothesis_instance.get_covid_data(location)
-        formatted_date = df.iloc[0]["Date_of_statistics"].strftime("%Y-%m-%d")
-        assert not df.empty
-        assert df.iloc[0]["Version"] == 2
-        assert df.iloc[0]["Date_of_report"] == "2022-01-03T09:00:00Z"
-        assert formatted_date == "2022-01-01"
-        assert df.iloc[0]["Security_region_code"] == "VR01"
-        assert df.iloc[0]["Security_region_name"] == "Groningen"
-        assert df.iloc[0]["Tested_with_result"] == 976
-        assert df.iloc[0]["Tested_positive"] == 378
 
-        # assert mock methods
-        mocked_get_file_paths.assert_called_once()
-        mocked_filter_dates.assert_called_once_with(mocked_get_file_paths.return_value)
-        mocked_read_csv.assert_called_once_with(
-            "https://github.com/mzelst/covid-19/raw/"
-            "master/data-rivm/tests/filtered_path",
-            compression="gzip",
-            header=0,
-            sep=",",
-            quotechar='"',
-        )
+        # Mock behavior of external dependencies
+        mock_exists.return_value = False
+        mock_download.return_value = mock_df
+        mock_read_csv.return_value = mock_df
+
+        out = hypothesis_instance.get_covid_data(municipality_code)
+
+        assert mock_download.called
+        assert not out.empty
+        assert out.iloc[0]["Version"] == 8
+        assert out.iloc[0]["Date_of_publication"] == "2022-01-01"
+        assert out.iloc[0]["Municipality_code"] == "GM0289"
+        assert out.iloc[0]["Municipality_name"] == "Wageningen"
+        assert out.iloc[0]["Total_reported"] == 6001
 
     def test_location_not_in_dataset(self):
         # trying to get data for an unknown location
@@ -135,17 +75,17 @@ class TestHypothesis:
             start = "2023-01-01"
             steps = 2
             hypothesis_instance = Hypothesis(start, steps)
-            hypothesis_instance.get_positive_cases("Los Angeles")
+            hypothesis_instance.get_positive_cases("XL4521")
 
     def test_scaling(self):
         # Tests the scale_cases_to_population function
-        real_size = 100
+        real_size = 1000
         sim_size = 50
-        daily_positive_cases = pd.Series([10, 20, 30, 40, 50])
+        cumulative_cases = pd.Series([100, 100, 130, 140, 200])
         out = Hypothesis.scale_cases_to_population(
-            daily_positive_cases, real_size, sim_size
+            cumulative_cases, real_size, sim_size
         )
-        expected = pd.Series([5, 10, 15, 20, 25])
+        expected = pd.Series([5, 0, 1, 0, 3])
         assert all(out == expected), f"Expected {expected}, but got {out}"
 
     def test_scaling_warning(self):
